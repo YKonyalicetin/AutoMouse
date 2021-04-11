1a0acc6c493ce340f40c5c517a9b99afa3954fec
yusuf.konyalicetin@gmail.com <yusuf.konyalicetin@gmail.com>
2021-04-11 23:05:44 +0200

    Add project files.

---
+++ AutoMouse/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace AutoMouse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft New Tai Lue", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(149, 71);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(103, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(423, 177);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "AutoMouse";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ AutoMouse/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoMouse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+            MoveCursor();
+            //InitTimer();
+
+        }
+        private void MoveCursor()
+        {
+            
+
+            Cursor MyCursor = new Cursor(Cursor.Current.Handle);
+
+            //very basic way of moving mouse up every up left
+            int random_numberX;
+            int random_numberY;
+
+            Random randmX = new Random();
+            random_numberX = randmX.Next(-10, 10);
+
+            Random randmY = new Random();
+            random_numberY = randmY.Next(-10, 10);
+
+            Cursor.Position = new Point (Cursor.Position.X + random_numberX, Cursor.Position.Y + random_numberY);
+           
+
+            //==========================Circular movement=================================
+            //double radius = 100.0d; //radius of the circle in pixels
+
+            //int startX = Cursor.Position.X - (int)radius;
+            //int startY = Cursor.Position.Y;
+            
+
+            
+            ////360° iteration
+            //for (double i = 0.0d; i < Math.PI * 2.0d; i += 0.1d)
+            //{
+            //    int x = startX + (int)(radius * Math.Cos(i));
+            //    int y = startY + (int)(radius * Math.Sin(i));
+
+            //    //set new mouse Position
+            //    Cursor.Position = new Point(Cursor.Position.X, Cursor.Position.Y);
+            //    //BFS.Input.SetMousePosition(x, y); this is original code coming from displayfusion.com; BFS package/library
+
+            //    //sleep for a bit
+            //    //BFS.General.Sleep(10);
+            //}
+
+
+            //this makes the mouse move only in rectangle defined by form borders
+            //Cursor.Clip = new Rectangle(this.Location, this.Size); 
+        }
+
+
+        private Timer timer1;
+        public void InitTimer()
+        {
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 60000; //30 seconds 30000 in millseconds
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MoveCursor();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitTimer();
+        }
+    }
+}
